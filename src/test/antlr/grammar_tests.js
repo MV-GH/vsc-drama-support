797ebf3c5746fddcf116a071bf1d793a864064e6
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-03-21 16:24:53 +0100

    Not all changes were committed

--- src/test/antlr/grammar_tests.js
+++ src/test/antlr/grammar_tests.js
@@ -2,31 +2,46 @@ import { exec } from "child_process";
 import { readdir } from 'fs';
 const rootTests = "./testfiles/"
 const validTests = rootTests + "valid/"
-const invalidTests = rootTests + "invalid";//These tests should fail to parse
+const invalidTests = rootTests + "invalid/";//These tests should fail to parse
 
 readdir(validTests, (err, files) => {
     if (err) {
-        throw Error(`Couldnt not read contents of ${rootTests + "valid/"}\n ${err}`)
+        throw Error(`Couldnt read contents of ${validTests}}\n ${err}`)
     }
     files.forEach(file => {
-        exec(`antlr4-parse ./definitions/DRAMA_Lexer.g4 ./definitions/drama.g4 start -gui ${validTests + file}`, (error, stdout, stderr) => {
-            if (error){
-                
+        exec(`antlr4-parse ./definitions/DRAMA_Lexer.g4 ./definitions/drama.g4 start -token ${validTests + file}`, (error, stdout, stderr) => {
+            if (error) {
+                throw Error(error.message)
+            }
+            if (stdout.length != 0) {
+                console.log(stdout)
+                console.log(stderr)
+                console.log(error)
+                throw Error(`Test "${file}" failed (should be valid)`)
             }
         })
-        console.log(file);
     });
+    console.log("All valid tests succeded 👍");
 });
 
 
-// exec("ls -la", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// });
+readdir(invalidTests, (err, files) => {
+    if (err) {
+        throw Error(`Couldnt read contents of ${invalidTests}\n ${err}`)
+    }
+    files.forEach(file => {
+        exec(`antlr4-parse ./definitions/DRAMA_Lexer.g4 ./definitions/drama.g4 start -token ${invalidTests + file}`, (error, stdout, stderr) => {
+            if (error) {
+                throw Error(error.message)
+            }
+            if (stdout.length == 0) {
+                console.log(stdout)
+                console.log(stderr)
+                console.log(error)
+                throw Error(`Test "${file}" failed (should be invalid)`)
+            }
+        })
+    });
+    console.log("All invalid tests succeded 👍");
+});
+
