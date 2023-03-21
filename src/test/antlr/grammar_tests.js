b9f6686b912dfd776dfb18b5b95249f562549e34
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-03-21 17:51:00 +0100

    And the changes

--- src/test/antlr/grammar_tests.js
+++ src/test/antlr/grammar_tests.js
@@ -1,47 +1,48 @@
-import { exec } from "child_process";
+import { promisify } from 'util';
+import { exec } from 'child_process'
+const exec_promise = promisify(exec);
 import { readdir } from 'fs';
+
 const rootTests = "./testfiles/"
 const validTests = rootTests + "valid/"
 const invalidTests = rootTests + "invalid/";//These tests should fail to parse
 
 readdir(validTests, (err, files) => {
     if (err) {
-        throw Error(`Couldnt read contents of ${validTests}}\n ${err}`)
+        throw Error(`couldn't read contents of ${validTests}}\n ${err}`)
     }
-    files.forEach(file => {
-        exec(`antlr4-parse ./definitions/DRAMA_Lexer.g4 ./definitions/drama.g4 start -token ${validTests + file}`, (error, stdout, stderr) => {
-            if (error) {
-                throw Error(error.message)
-            }
-            if (stdout.length != 0) {
-                console.log(stdout)
-                console.log(stderr)
-                console.log(error)
-                throw Error(`Test "${file}" failed (should be valid)`)
-            }
-        })
-    });
-    console.log("All valid tests succeded 👍");
+    Promise.all(
+        files.map(file => {
+            const prom = exec_promise(`antlr4-parse -v 4.12.0 ./definitions/DRAMA_Lexer.g4 ./definitions/drama.g4 start -token ${validTests + file}`);
+
+            prom.child.stdout.on("data", stdout => {
+                if (stdout.length != 0) {
+                    console.log(stdout)
+                    throw Error(`Test "${file}" failed (should be valid)`)
+                }
+            })
+            return prom;
+        }))
+        .then(_ => { console.log("All valid tests succeeded 👍"); })
+
 });
 
 
 readdir(invalidTests, (err, files) => {
     if (err) {
-        throw Error(`Couldnt read contents of ${invalidTests}\n ${err}`)
+        throw Error(`couldn't read contents of ${invalidTests}}\n ${err}`)
     }
-    files.forEach(file => {
-        exec(`antlr4-parse ./definitions/DRAMA_Lexer.g4 ./definitions/drama.g4 start -token ${invalidTests + file}`, (error, stdout, stderr) => {
-            if (error) {
-                throw Error(error.message)
-            }
-            if (stdout.length == 0) {
-                console.log(stdout)
-                console.log(stderr)
-                console.log(error)
-                throw Error(`Test "${file}" failed (should be invalid)`)
-            }
-        })
-    });
-    console.log("All invalid tests succeded 👍");
-});
+    Promise.all(
+        files.map(file => {
+            const prom = exec_promise(`antlr4-parse -v 4.12.0 ./definitions/DRAMA_Lexer.g4 ./definitions/drama.g4 start -token ${invalidTests + file}`);
 
+            prom.child.stdout.on("data", stdout => {
+                if (stdout.length === 0) {
+                    throw Error(`Test "${file}" failed (should be invalid)`)
+                }
+            })
+            return prom;
+        }))
+        .then(_ => { console.log("All invalid tests succeeded 👍"); })
+
+});
